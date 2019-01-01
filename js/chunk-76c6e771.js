d9b292f1d91bf359bfbc65e479b0ac6fe23921a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-01 05:39:16 +0000

    Deploy NotWoods/color-breakdown to github.com/NotWoods/color-breakdown.git:gh-pages

--- js/chunk-76c6e771.js
+++ js/chunk-76c6e771.js
@@ -1,2 +1,2 @@
-function writeText(e){return new Promise((t,n)=>{let o=!1;document.addEventListener("copy",t=>{t.clipboardData.setData("text/plain",e),t.preventDefault(),o=!0},{once:!0}),document.execCommand("copy"),o?t():n()})}export{writeText};
+function e(e){return new Promise((t,n)=>{let o=!1;document.addEventListener("copy",t=>{t.clipboardData.setData("text/plain",e),t.preventDefault(),o=!0},{once:!0}),document.execCommand("copy"),o?t():n()})}export{e as writeText};
 //# sourceMappingURL=chunk-76c6e771.js.map
