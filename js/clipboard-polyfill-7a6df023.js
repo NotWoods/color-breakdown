5cc377f505764a2360cfcbca30cb3c0c6efa9619
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-20 06:37:19 +0000

    Deploy NotWoods/color-breakdown to github.com/NotWoods/color-breakdown.git:gh-pages

--- js/clipboard-polyfill-7a6df023.js
+++ js/clipboard-polyfill-7a6df023.js
@@ -1,2 +1,2 @@
 function e(e){return new Promise((t,n)=>{let o=!1;document.addEventListener("copy",t=>{t.clipboardData.setData("text/plain",e),t.preventDefault(),o=!0},{once:!0}),document.execCommand("copy"),o?t():n()})}export{e as writeText};
-//# sourceMappingURL=clipboard-polyfill-b051499f.js.map
+//# sourceMappingURL=clipboard-polyfill-7a6df023.js.map
